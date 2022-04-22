5d04d7d15db1ea26e9d3adead6a6ab53ff6c53f2
pagioro <pagioro@hotmail.com>
2022-04-22 11:20:59 +0000

    Calculate stock number and add to stock worksheet

--- run.py
+++ run.py
@@ -55,8 +55,7 @@ def validate_data(values):
 
     return True
 
-
-"""
+    """
 Update sales worksheet, add new row with the list data provided
 def update_sales_worksheet(data):  
     
@@ -74,7 +73,6 @@ def update_surplus_worksheet(data):
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
 """
-
 
 def update_worksheet(data, worksheet):
     """
@@ -105,19 +103,37 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
 def get_last_5_entries_sales():
     """
-    Collects colummns of data from sales worksheet, collecting
-    the last 5 entries for each sandwich and returns the data as a list of lists
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
     """
     sales = SHEET.worksheet("sales")
+
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
 
     return columns
-    
+
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -129,9 +145,11 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
 
-sales_columns = get_last_5_entries_sales()
